91e65d85a7b55e37fd3d058419c54f1721d90f88
Fraser <fraser.cam088@gmail.com>
2021-04-04 22:56:02 +0100

    Made by jack

---
+++ snx_loadingscreen_final/__resource.lua
@@ -0,0 +1,12 @@
+description 'snx_loadingscreen if you have an issue contact me:SnorlaX#0090 '
+
+files {
+    'index.html',
+    'css/style.css',
+    'img/logo.png',
+    'music/loading.ogg',
+
+
+}
+
+loadscreen 'index.html'
